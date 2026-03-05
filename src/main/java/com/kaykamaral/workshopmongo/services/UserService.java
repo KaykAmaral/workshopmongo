b70f7cde8706bbd880e92c6863ab7bb3d806554c
KaykAmaral <kayaquiurbano@gmail.com>
2026-03-05 03:14:38 -0300

    User FindById & Exceptions

--- src/main/java/com/kaykamaral/workshopmongo/services/UserService.java
+++ src/main/java/com/kaykamaral/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.kaykamaral.workshopmongo.services;
 
 import com.kaykamaral.workshopmongo.domain.User;
 import com.kaykamaral.workshopmongo.repository.UserRepository;
+import com.kaykamaral.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,6 +17,11 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
     }
 
 }
