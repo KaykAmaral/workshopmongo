26c1afe42e39b814ec200e104b580263240ceb27
KaykAmaral <kayaquiurbano@gmail.com>
2026-03-05 11:36:07 -0300

    User Update

--- src/main/java/com/kaykamaral/workshopmongo/services/UserService.java
+++ src/main/java/com/kaykamaral/workshopmongo/services/UserService.java
@@ -32,6 +32,18 @@ public class UserService {
         repo.deleteById(id);
     }
 
+    public void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+
     public User fromDTO(UserDTO objDTO) { return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail()); }
 
 }
