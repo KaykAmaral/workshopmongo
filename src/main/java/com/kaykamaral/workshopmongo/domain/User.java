961448bcbc5f824586f24af35cbc575074672518
KaykAmaral <kayaquiurbano@gmail.com>
2026-03-06 21:03:15 -0300

    Making References User's Posts

--- src/main/java/com/kaykamaral/workshopmongo/domain/User.java
+++ src/main/java/com/kaykamaral/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.kaykamaral.workshopmongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -17,6 +20,9 @@ public class User implements Serializable {
     private String name;
     private String email;
 
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
     public User() {}
     public User(String id, String name, String email) {
         this.id = id;
@@ -27,10 +33,12 @@ public class User implements Serializable {
     public void  setId(String id) { this.id = id; }
     public void setName(String name) { this.name = name; }
     public void setEmail(String email) { this.email = email; }
+    public void setPosts(List<Post> posts) { this.posts = posts; }
 
     public String getId() { return id; }
     public String getName() { return name; }
     public String getEmail() { return email; }
+    public List<Post> getPosts() { return posts; }
 
     @Override
     public boolean equals(Object o) {
